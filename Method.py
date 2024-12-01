f38690148a9776228426f2d1d126f23a74332e8a
mahmoudmarey226 <mahmoud.man606@gmail.com>
2024-12-01 05:16:39 +0200

    added print lines for step by step explaination except for LU

--- Method.py
+++ Method.py
@@ -19,15 +19,29 @@ class Equations():
          self.j += 1  
  
 class Method():
-    def __init__(self,coff,solu,sig):
+    def __init__(self,coff,solu,sig , step_by_step = False):
         self.coff = coff
         self.sol =  solu
         self.sig = sig
+        self.step_by_step = step_by_step
+
+        # if self.step_by_step:
+        #     print("a = ")
+        #     print(self.coff)
+        #     print("b = ")
+        #     print(self.sol)
 
     def __str__(self):
         return f"{self.coff} = {self.sol}"
     
     def pivoting(self,i,j):
+        if self.step_by_step:
+            print(f"**** Pivoting on a{i}{j} start ****")
+            print("a = ")
+            print(self.coff)
+            print("b = ")
+            print(self.sol)
+
         row = i
         max = self.coff[i,j]
         for k in range(i,len(self.coff)):
@@ -37,27 +51,64 @@ class Method():
         self.coff[[i, row], :] = self.coff[[row, i], :]
 
         self.sol[i],self.sol[row] = self.sol[row],self.sol[i]
+
+        if self.step_by_step:
+            print(f"After pivoting on a{i}{j}")
+            print("a = ")
+            print(self.coff)
+            print("b = ")
+            print(self.sol)
+            print(f"**** Pivoting on a{i}{j} end ****")
     
     
-    def forwardElemination(self,i,j):                 
+    def forwardElemination(self,i,j):             
+        if self.step_by_step:
+            print(f"**** Forward elemination on a{i}{j} start ****")
+            print("a = ")
+            print(self.coff)
+            print("b = ")
+            print(self.sol)
+
         self.pivoting(i,j)
         pivot = self.coff[i, i]
         if pivot == 0:
+            if self.step_by_step:
+                print(f"pivot = 0 :( ")
+                print(f"**** Forward elemination on a{i}{j} end ****")
             return False
         for k in range (i+1,len(self.coff)):
+            
             m = self.coff[k,j]/self.coff[i,j]
             self.coff[k,j] =0
             self.sol[k] -= m*self.sol[i]
             self.sol[k]= self.sign(self.sol[k])
+
             for l in range(j+1,len(self.coff)):
                 self.coff[k,l] -= m*self.coff[i,l]
                 self.coff[k,l]= self.sign(self.coff[k,l]) 
+            
+            if self.step_by_step:
+                print(f"after forward elemination on a{i}{j} with m = {m}")
+                print("a = ")
+                print(self.coff)
+                print("b = ")
+                print(self.sol)
         if(i == len(self.coff)-1 and j == len(self.coff)-1):
+            if self.step_by_step:
+                print(f"**** Forward elemination on a{i}{j} end ****")
             return True
-        else:        
+        else:
+           if self.step_by_step:
+                print(f"**** Forward elemination on a{i}{j} end ****")        
            return self.forwardElemination(i+1,j+1)
 
     def backSub(self):
+        if self.step_by_step:
+            print("**** Backward Substitution start ****")
+            print("a = ")
+            print(self.coff)
+            print("b = ")
+            print(self.sol)
 
         x = np.zeros(len(self.sol)) 
 
@@ -71,7 +122,13 @@ class Method():
         
 
             x[k] =self.sign( sum / self.coff[k, k])
+            if self.step_by_step:
+                print(f"X{k+1} = {x[k]}")
 
+
+        if self.step_by_step:
+            print(f"Solution = {x}")
+            print("**** Backward Substitution end ****")
         return x
 
     def sign (self,value):
